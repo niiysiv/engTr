88baffb1172777c463206a0d47caea1ab47558ee
niiysiv <75676836+niiysiv@users.noreply.github.com>
2021-06-16 08:35:12 +0900

    Create english.js

---
+++ english.js
@@ -0,0 +1,30 @@
+'use strict';
+
+let popupImage = function()
+{
+    let popup = document.getElementById("js-popup");
+    if(!popup) return;
+
+    let blackBg = document.getElementById("js-black-bg"); 
+    let closeBtn = document.getElementById("js-close-btn");
+    let showBtn = document.getElementById("js-show-popup");
+
+    closePopUp(blackBg);
+    closePopUp(closeBtn);
+    closePopUp(showBtn);
+
+    function closePopUp(elem) 
+    {
+        if(!elem) return;
+        elem.addEventListener('click', function() 
+        {
+        popup.classList.toggle('is-show');
+        });
+    }
+}
+popupImage();
+
+$(function()
+{
+    $('div').hide().fadeIn(1000)
+});
